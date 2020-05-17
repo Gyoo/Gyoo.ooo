e40646b7565900ce45d059d168e1b0e40e391fa4
Gaétan Young <gyozell@gmail.com>
2020-05-17 16:09:54 +0200

    Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import localeFr from '@angular/common/locales/fr';
+import {registerLocaleData} from '@angular/common';
+import {TranslateService} from '@ngx-translate/core';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'GyooDotOoo';
+
+  @ViewChild('navBurger', {static: true}) navBurger: ElementRef;
+  @ViewChild('navMenu', {static: true}) navMenu: ElementRef;
+
+  public language = localStorage.getItem('language') ? localStorage.getItem('language') : navigator.language.split('-')[0];
+
+  public availableLocales = ['en', 'fr'];
+
+  public faGithub = faGithub;
+
+  constructor(private translate: TranslateService) {
+    registerLocaleData(localeFr, 'fr');
+    translate.setDefaultLang('en');
+    if (this.availableLocales.includes(this.language)) {
+      this.useLanguage(this.language);
+    } else {
+      this.useLanguage('en');
+    }
+  }
+
+  useLanguage(language: string) {
+    this.language = language;
+    localStorage.setItem('language', language);
+    this.translate.use(language);
+  }
+
+  toggleNavbar() {
+    this.navBurger.nativeElement.classList.toggle('is-active');
+    this.navMenu.nativeElement.classList.toggle('is-active');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,56 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {WebpackTranslateLoader} from '../loader/webpack-translate-loader';
+import {RouterModule, Routes} from '@angular/router';
+import {ResumeComponent} from './resume/resume.component';
+import {ProjectsComponent} from './projects/projects.component';
+import {HobbiesComponent} from './hobbies/hobbies.component';
+import {LandingComponent} from './landing/landing.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: LandingComponent
+  },
+  {
+    path: 'resume',
+    component: ResumeComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  {
+    path: 'hobbies',
+    component: HobbiesComponent
+  },
+  {path: '**', redirectTo: '/', pathMatch: 'full'}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResumeComponent,
+    ProjectsComponent,
+    HobbiesComponent,
+    LandingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FontAwesomeModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useClass: WebpackTranslateLoader
+      }
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
